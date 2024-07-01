bf742dd8b2a0788c74a80ec454d12cd6f992bc1c
Aishlia <nai.julia@gmail.com>
2024-07-01 14:45:45 -0700

    Remove package-lock

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ const GNSTradingV6_1 = artifacts.require("GNSTradingV6_1");
 const GNSTradingCallbacksV6_1 = artifacts.require("GNSTradingCallbacksV6_1");
 
 module.exports = function(deployer) {
-  deployer.deploy(GNSPairInfosV6_1,"");
-  deployer.deploy(GNSTradingCallbacksV6_1,"","");
-  deployer.deploy(GNSTradingV6_1,"","");
+  deployer.deploy(GNSPairInfosV6_1,"0x006042665d8F2A165630619a8eA2AE6376714Eb2");
+  deployer.deploy(GNSTradingCallbacksV6_1,"0x006042665d8F2A165630619a8eA2AE6376714Eb2","0x006042665d8F2A165630619a8eA2AE6376714Eb2");
+  deployer.deploy(GNSTradingV6_1,"0x006042665d8F2A165630619a8eA2AE6376714Eb2","0x006042665d8F2A165630619a8eA2AE6376714Eb2");
 
 };
